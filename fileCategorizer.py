e799b51ac5f464a2cb009fb6012204c362f8b981
epicexcelsior <legoawesome@icloud.com>
2023-01-10 20:58:11 -0900

    Update fileCategorizer.py
    - breaks file opening into phases: 1st tries VLC, then browser (using HTML file), then default program
    - includes non-exhaustive hard-coded lists to determine which formats can be opened by VLC/browsers

--- fileCategorizer.py
+++ fileCategorizer.py
@@ -80,31 +80,61 @@ def inputValidation(prompt, iterable):
     return choice
 
 
+# Checks if a file's extension is included in a provided list
+def extensionInList(filepath, iterable):
+    _, extension = os.path.splitext(filepath)
+    extension = extension[1:]  # remove the first . from the extension
+    return extension in iterable # True if it's in the list, false if not
+
+
+vlcFormats = ['mp4', 'webm', 'jpeg', 'jpg', 'png', '3g2', '3gp', '3gp2', '3gpp', 'amv', 'asf', 'avi', 'bik', 'flv', 'gxf', 'm1v',
+'m2v', 'm2t', 'm2ts', 'm4v', 'mkv', 'moov', 'mov', 'mpeg', 'mpeg1', 'mpeg2', 'mpeg4',
+'mpg', 'mpv', 'mt2s', 'mts', 'mxf', 'nsv', 'ogm', 'ogv', 'qt', 'rm', 'rmvb', 'swf', 'ts', 'vob',
+'wmv', '3ga', 'aac', 'ac3', 'aif', 'aiff', 'amr', 'ape', 'au', 'flac', 'm4a', 'm4b', 'm4p',
+'mid', 'midi', 'mka', 'mp3', 'mpa', 'ogg', 'ra', 'wav', 'weba', 'bmp', 'ico', 'tiff',
+'mms', 'rtp', 'rtsp', 'sdp']
+browserFormats = ['gif', 'webp', 'apng']
+
+
 createFolders()
 # Print list of valid options
 print("Welcome to the file categorizer.\nValid options:")
 for key in validOptions.keys():
     print(key, '-', validOptions[key])
-print("\n")
+print("Q - quit\n")
 
 
+browserFiles = [] # Paths to files that can't be opened with VLC (to be opened later in a web browser)
+
+# PHASE 1 - Open files using VLC
 # "Walk" through all files in folder & subfolders
 for (subdir, dirs, files) in os.walk(sourceFolder):
     for f in files:
         filePath = os.path.join(subdir, f) # Joins path of current subdirectory with current file
-        try:
-            p = subprocess.run([vlcPath, filePath]) # Open file using VLC executable
-        except:
-            print(f"Failed to open '{f}'")
+        
+        # If filepath is in vlcFormats, deal with it now
+        # Else add it to later list
+        if extensionInList(filePath, vlcFormats):
+            try:
+                # Open file using VLC executable
+                p = subprocess.Popen([vlcPath, filePath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            except:
+                print(f"Failed to open '{filePath}'")
+                browserFiles.append(filePath)
+                continue
 
-        # Get user input (forces valid input)
-        operation = inputValidation("Enter category: ", list(validOptions.keys()) + ['q'])
+            # Get user input (forces valid input)
+            operation = inputValidation("Enter category: ", list(validOptions.keys()) + ['q'])
 
-        # Exit loop upon user input
-        if operation == 'q': break
+            # Exit loop upon user input
+            if operation == 'q': break
 
-        # Perform operations according to user input
-        fileOperations(filePath, operation)
+            # Perform operations according to user input
+            fileOperations(filePath, operation)
+        else:
+            browserFiles.append(filePath)
+            print(f'{filePath} can\'t be opened by VLC. It will be opened by the default program at the end.')
+            continue
 
 
 # Close VLC
@@ -113,6 +143,47 @@ try:
     p.terminate()
 except:
     print("Unable to close VLC.")
+
+
+laterFiles = [] # Paths to files that can't be opened with a browser (to be opened later with default program)
+# PHASE 2 - Open files using HTML & web browser
+print('\nNow viewing files that could not be opened by VLC.\n')
+for f in browserFiles:
+    if extensionInList(f, browserFormats):
+        try:
+            open(f"{f}.html", "w").write(f"<html><body><img src='{f}'></body></html>")
+            webbrowser.open_new_tab(f"{f}.html")
+        except:
+            print(f"Failed to open '{f}'")
+            continue
+
+        operation = inputValidation("Enter category: ", list(validOptions.keys()) + ['q'])
+
+        if operation == 'q':
+            os.remove(f'{f}.html')
+            break
+
+        fileOperations(f, operation)
+        os.remove(f'{f}.html')
+    else:
+        laterFiles.append(f)
+
+
+# PHASE 3 - Open files using default program
+print('\nNow viewing files that could not be opened by VLC or browser.\n')
+for filePath in laterFiles:
+    try:
+        # Open file using default program
+        os.startfile(filePath)
+    except:
+        print(f"Failed to open '{filePath}'")
+        continue
+
+    operation = inputValidation("Enter category: ", list(validOptions.keys()) + ['q'])
+
+    if operation == 'q': break
+
+    fileOperations(filePath, operation)
 
 
 # Confirms deletion of files in specified directory, then deletes all directory contents
